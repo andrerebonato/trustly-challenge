8fb74ad6bc1d0694a8c116d3438d1d9cf3db7399
andrerebonato <github.user>
2020-09-28 21:16:14 -0300

    Refactored test

--- test/scrappingController.spec.js
+++ test/scrappingController.spec.js
@@ -4,34 +4,30 @@ const chai = require("chai");
 const expect = chai.expect;
 const url = "https://rebonato-trustly-challenge-api.herokuapp.com/repository/files/info/andrerebonato/boiler-rest-api-nodets";
 
-describe("Test challenger Trustly API", async () => {
-    it("Must show all lines of files grouped by file extension", async (done) => {
+describe("Test challenger Trustly API", () => {
+    it("Must show all lines of files grouped by file extension", async () => {
         const response = await axios.get(url);
         const { data } = response;
 
         expect(response.status).to.equal(200);
 
-        if(data.should.have.property('model')){
-            expect(data.model.json.lines).to.equal(3871)
-            expect(data.model.js.lines).to.equal(31);
-            expect(data.model.text.lines).to.equal(2984);
-            expect(data.model.ts.lines).to.equal(247);
-        }
-        done();
-        
+        expect(data).to.have.property('model');
+        expect(data.model.json.lines).to.equal(3871)
+        expect(data.model.js.lines).to.equal(31);
+        expect(data.model.text.lines).to.equal(2984);
+        expect(data.model.ts.lines).to.equal(247);
     });
-    it("Must show all total files grouped by file extension", async (done) => {
+    it("Must show all total files grouped by file extension", async () => {
         const response = await axios.get(url);
         const { data } = response;
 
         expect(response.status).to.equal(200);
 
-        if(data.should.have.property('model')){
-            expect(data.model.json.files).to.equal(3)
-            expect(data.model.js.files).to.equal(2);
-            expect(data.model.text.files).to.equal(2);
-            expect(data.model.ts.files).to.equal(14);
-        }
-        done();
+        expect(data).to.have.property('model');
+        expect(data.model.json.files).to.equal(3)
+        expect(data.model.js.files).to.equal(2);
+        expect(data.model.text.files).to.equal(2);
+        expect(data.model.ts.files).to.equal(14);
+        
     });
 });
